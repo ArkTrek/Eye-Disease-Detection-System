47a147244ff605ff657b6978e14b0d57e04b5996
Arpit R <108040880+ArkTrek@users.noreply.github.com>
2024-05-22 15:07:37 +0530

    Delete TelegramBotConfig.py

--- TelegramBotConfig.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-bot_token = '7109066009:AAF5dilbMQrgxd6fdHvRvOGPas-yBBLfvYQ'
-CID = "1453432519"
-msg = "Booking Succesfully Made!"
-
-url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
-print(requests.get(url).json())
